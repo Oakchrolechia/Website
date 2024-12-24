bdaa9f25b573038467e0a143210c72c323ac66de
Oakchrolechia <88589220+Oakchrolechia@users.noreply.github.com>
2024-12-24 10:49:28 +0000

    Navbar and ReactViteTsx Updates

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,32 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { createHtmlPlugin } from "vite-plugin-html";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    createHtmlPlugin({
+      inject: {
+        data: {
+          head: `
+            <meta charset="utf-8" />
+            <meta
+              name="description"
+              content="Unofficial Crusader Patch Website; Offering stability and bug fixes, AI & gameplay enhancements, and improving the multiplayer experience for Stronghold Crusader."
+            />
+            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+            <title>Unofficial Crusader Patch</title>
+            <link rel="stylesheet" href="/src/css/app.css" />
+            <link
+              rel="stylesheet"
+              href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css"
+            />
+            <link rel="icon" type="image/png" href="/UCP3-Logo-32.png" />
+          `,
+        },
+      },
+    }),
+  ],
+  root: "./",
+  publicDir: "public",
+});
