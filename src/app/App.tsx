1d72f6968ea7cd67f0a50dcf3a738a666bee6668
Oakchrolechia <88589220+Oakchrolechia@users.noreply.github.com>
2024-12-26 11:34:22 +0000

    Organising

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,5 +1,5 @@
-import "./css/App.css";
-import { Index } from "../pages/PortedIndex";
+import "./app/App.css";
+import { Index } from "../pages/PortedIndex.tsx";
 import { Route, Routes } from "react-router-dom";
 import { Navbar } from "./Navbar.tsx";
 
